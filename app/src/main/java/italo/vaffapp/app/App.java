ae9f069337bc19bc4e5357b4c2e41d17f426a5f3
Italo Armenti <iarmenti@fiksu.com>
2015-02-10 08:31:32 +0200

    added App class

---
+++ app/src/main/java/italo/vaffapp/app/App.java
@@ -0,0 +1,25 @@
+package italo.vaffapp.app;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by iarmenti on 2/9/15.
+ */
+public class App {
+    public final String text;
+    public final String package_name;
+    public final Drawable icon;
+    public App(String text, String package_name, Drawable icon) {
+        this.text = text;
+        this.package_name = package_name;
+        this.icon = icon;
+    }
+
+    public String getPackageName(){
+        return this.package_name;
+    }
+    @Override
+    public String toString() {
+        return text;
+    }
+}
